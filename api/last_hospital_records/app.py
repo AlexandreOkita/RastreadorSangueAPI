0301bc4237ce965ff1989827b5cc5ba4463a33ea
AlexandreOkita <xande.okita@gmail.com>
2024-02-12 11:17:23 -0300

    return last items if no hospital is defined

--- api/last_hospital_records/app.py
+++ api/last_hospital_records/app.py
@@ -3,10 +3,8 @@ from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('your_table_name')
+    table = dynamodb.Table('hospitals_blood_status')
 
-    hospital = event['queryStringParameters']['hospital']
-    
     limit = int(event['queryStringParameters'].get('limit', 10))  # default to 10 if not provided
     if limit < 1 or limit > 100:
         return {
@@ -14,11 +12,22 @@ def lambda_handler(event, context):
             'body': 'Limit must be between 1 and 100'
         }
 
-    response = table.query(
-        KeyConditionExpression=Key('hospital').eq(hospital),
-        Limit=limit,
-        ScanIndexForward=False
-    )
+    if 'hospital' in event['queryStringParameters']:
+        response = table.query(
+            KeyConditionExpression=Key('hospital').eq(event['queryStringParameters']['hospital']),
+            Limit=limit,
+            ScanIndexForward=False
+        )
+        if not response['Items']:
+            return {
+                'statusCode': 404,
+                'body': 'Hospital not found'
+            }
+    else:
+        response = table.scan(
+            Limit=limit,
+            ScanIndexForward=False
+        )
 
     items = response['Items']
 
